2e96a6d0dc864c9d8dd57a86ac7d2e6cade71a86
bhavyasssweety <sankubhavyasri@gmail.com>
2023-03-31 23:39:06 -0400

    updated code

--- main/main.go
+++ main/main.go
@@ -12,5 +12,6 @@ func main() {
 	http.HandleFunc("/sign-up", userservice.Signup)
 	http.HandleFunc("/login", userservice.Login)
 	http.HandleFunc("/changepassword", userservice.Chanpassword)
+	http.HandleFunc("/changepassword", userservice.Home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
